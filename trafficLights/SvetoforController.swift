a65c8ed83057e8b8e598a7e49395a45baa3be8bd
MAKSIM89PW <123>
2023-06-20 13:16:34 +0300

    addStackView

--- trafficLights/SvetoforController.swift
+++ trafficLights/SvetoforController.swift
@@ -14,21 +14,42 @@ class SvetoforController: UIViewController {
     var secondSectionView = SvetoforSectionView(colorLight: .orange)
     var thirdSectionView = SvetoforSectionView(colorLight: .green)
     
+    let stackView = UIStackView(frame: .zero)
     //кнопка
     @IBOutlet weak var turnButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(firstSectionView)
-        view.addSubview(secondSectionView)
-        view.addSubview(thirdSectionView)
+        view.addSubview(stackView)
+        stackView.spacing = 0
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        
+        stackView.addArrangedSubview(firstSectionView)
+        stackView.addArrangedSubview(secondSectionView)
+        stackView.addArrangedSubview(thirdSectionView)
+        
+        stackView.snp.makeConstraints { make in
+            make.width.equalTo(300)
+            make.height.equalTo(300)
+            make.top.equalTo(64)
+            make.centerX.equalToSuperview()
+        }
         
         firstSectionView.snp.makeConstraints { make in
             make.width.height.equalTo(100)
-            make.centerX.equalToSuperview()
-            make.top.equalTo(100)
         }
+        
+        secondSectionView.snp.makeConstraints { make in
+            make.width.height.equalTo(100)
+        }
+
+        thirdSectionView.snp.makeConstraints { make in
+            make.width.height.equalTo(100)
+        }
+
         
         firstSectionView.turnOff()
         secondSectionView.turnOff()
